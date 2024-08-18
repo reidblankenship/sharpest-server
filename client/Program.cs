ec1cf7168085c866b3bdaae22908448146f471d0
reidblankenship <reid.o.blankenship@proton.me>
2024-08-18 11:22:06 -0700

    initial commit

---
+++ client/Program.cs
@@ -0,0 +1,108 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+
+namespace client;
+
+class Client
+{
+    public static void Main()
+    {
+        RunningTcpClient runner = new RunningTcpClient();
+        if (!runner.Startable)
+        {
+            Console.Error.WriteLine("Client wasn't startable. Terminating...");
+            return;
+        }
+        Console.WriteLine("Start conversation? (n/Y)");
+        var input = Console.ReadLine();
+        if (input == "Y" || input == "y")
+        {
+            runner.MaintainConnection();
+        }
+        else
+        {
+            runner.Stop();
+        }
+    }
+}
+
+class RunningTcpClient
+{
+    private TcpClient client;
+    private NetworkStream? stream;
+    public bool Startable = false;
+    public RunningTcpClient()
+    {
+        client = new TcpClient();
+        try
+        {
+            client.Connect(IPAddress.Parse("127.0.0.1"), 8888);
+            stream = client.GetStream();
+        }
+        catch (ObjectDisposedException ode)
+        {
+            Console.WriteLine("TCP Server not found. (Are you sure you're typing it in correctly?)");
+            Console.Error.WriteLine(ode.Message);
+        }
+        catch (SocketException se)
+        {
+            Console.WriteLine("Socket Error.");
+            Console.Error.WriteLine(se.Message);
+        }
+
+        if (client.Connected)
+        {
+            Console.WriteLine("Connected to server at: 127.0.0.1:8888");
+            Startable = true;
+        }
+    }
+
+
+    public void MaintainConnection()
+    {
+        while (client.Connected)
+        {
+            try
+            {
+                string? message = Console.ReadLine() ?? "Stop.";
+                string messageReceived = SendAndReceive(message);
+                Console.WriteLine($"Message from Server: {messageReceived}");
+                if (message == "Stop.")
+                {
+                    break;
+                }
+            }
+            catch (ThreadInterruptedException tie)
+            {
+                Console.Error.WriteLine(tie.Message);
+                break;
+            }
+        }
+        Stop();
+    }
+
+    public void Stop()
+    {
+        stream?.Close();
+        client.Close();
+    }
+
+    private string SendAndReceive(string sent)
+    {
+        byte[] sentData = Encoding.UTF8.GetBytes(sent);
+        stream?.Write(sentData, 0, sentData.Length);
+
+        byte[] receivedData = new byte[1024];
+        int bytesRead = stream?.Read(receivedData, 0, receivedData.Length) ?? -1;
+        if (bytesRead == -1)
+        {
+            Console.Error.WriteLine("Error occurred with NetworkStream. Terminating...");
+            throw new Exception();
+        }
+
+        string received = Encoding.UTF8.GetString(receivedData, 0, bytesRead);
+        return received;
+    }
+}
---
+++ listener/Program.cs
@@ -0,0 +1,135 @@
+﻿namespace listener;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+class Listener
+{
+    static void Main()
+    {
+        TcpServer server = new TcpServer();
+    }
+}
+
+class TcpServer
+{
+
+    private NetworkStream? stream;
+    private TcpListener listener;
+    private TcpClient? client;
+    private bool connected;
+
+
+    public TcpServer()
+    {
+
+        listener = new TcpListener(IPAddress.Parse("127.0.0.1"), 8888);
+        try
+        {
+
+            listener.Start();
+            Console.WriteLine("Server listening on 127.0.0.1:8888....");
+            client = listener.AcceptTcpClient();
+        }
+        catch (SocketException se)
+        {
+            Console.Error.WriteLine(se.Message);
+            Console.Error.WriteLine("Socket Error Code = " + se.ErrorCode);
+            return;
+        }
+        Console.WriteLine("Client Connected.");
+        Console.WriteLine("Client Receive Timeout = " + client.ReceiveTimeout);
+        Console.WriteLine("Client Send Timeout = " + client.SendTimeout);
+
+        stream = client.GetStream();
+
+        connected = true;
+        while (connected)
+        {
+            string response = GetMessage();
+            Console.WriteLine($"Received: {response}");
+            if (response == "Stop.")
+            {
+                break;
+            }
+            Respond(ProcessMessage(response));
+        }
+        Stop();
+    }
+
+    public void Stop()
+    {
+        stream?.Close();
+        client?.Close();
+        listener.Stop();
+    }
+
+    string GetMessage()
+    {
+        if (stream == null)
+        {
+            connected = false;
+            return "Stop.";
+        }
+        byte[] buffer = new byte[1024];
+        int bytesRead = -1;
+        try
+        {
+            bytesRead = stream.Read(buffer, 0, buffer.Length);
+        }
+        catch (Exception e)
+        {
+            Console.Error.WriteLine(e.Message);
+            Console.Error.WriteLine(e.StackTrace);
+        }
+        if (bytesRead == -1)
+        {
+            Console.Error.WriteLine("No bytes were read from the NetworkStream.");
+            throw new Exception();
+        }
+        string receivedData = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+        return receivedData;
+    }
+
+    void Respond(string response)
+    {
+        if (stream == null)
+        {
+            connected = false;
+            return;
+        }
+        byte[] responseData = Encoding.UTF8.GetBytes(response);
+        stream.Write(responseData, 0, responseData.Length);
+    }
+
+    static string ProcessMessage(string received)
+    {
+        if (received == null)
+        {
+            return "Foo, Bar.";
+        }
+        string result = "";
+        switch (received)
+        {
+            case "Stop.":
+                result = "We understand. Have a nice day.";
+                break;
+            case "Hello.":
+                result = "Hi, how are you?";
+                break;
+
+            case "Goodbye.":
+                result = "Fun talking with you.";
+                break;
+            default:
+                result = reverse(received);
+                break;
+        }
+        return result;
+    }
+
+    static string reverse(string input)
+    {
+        return new string(input.ToCharArray().Reverse().ToArray());
+    }
+}
